16bb06a97ee98aa071ba8c965416209fb9fa8200
Murilo Barbosa <barbosamurilo023@gmail.com>
2023-08-28 08:57:21 -0300

    exemplos de tipos de variáveis e constantes e a utilização de casting

--- tiposVariaveis/TiposVariaveis.java
+++ tiposVariaveis/TiposVariaveis.java
@@ -4,6 +4,17 @@ public class TiposVariaveis {
 
     public static void main(String[] args) {
         System.out.println ("Hello World!");
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short getNumeroCurto2 = (short) numeroNormal;
+        //casting
+        int numero = 5;
+        numero = 10;
+        System.out.println (numero);
+
+        final double VALOR_DE_PI = 3.14;
+            System.out.println (VALOR_DE_PI);
     }
 
 }
